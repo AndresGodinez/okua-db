7f774e225162d0cf8c2dca80572657a94d0cc717
Carlos Hernandez <carlos.hernandez@connectit.com.mx>
2018-09-03 10:52:29 -0500

    Added index support for migration change_s02_cfdi_taxes_cfdi_id_column

--- deploy/change_s02_cfdi_taxes_cfdi_id_column.sql
+++ deploy/change_s02_cfdi_taxes_cfdi_id_column.sql
@@ -2,6 +2,10 @@
 
 BEGIN;
 
+alter table s02_cfdi_taxes drop index s02_cfdi_taxes_bill_info_id_index;
+
 alter table s02_cfdi_taxes change bill_info_id cfdi_id bigint(19) not null;
+
+create index s02_cfdi_taxes_cfdi_id_index on s02_cfdi_taxes (cfdi_id);
 
 COMMIT;
--- revert/change_s02_cfdi_taxes_cfdi_id_column.sql
+++ revert/change_s02_cfdi_taxes_cfdi_id_column.sql
@@ -2,6 +2,10 @@
 
 BEGIN;
 
+alter table s02_cfdi_taxes drop index s02_cfdi_taxes_cfdi_id_index;
+
 alter table s02_cfdi_taxes change cfdi_id bill_info_id bigint(19) not null;
+
+create index s02_cfdi_taxes_bill_info_id_index on s02_cfdi_taxes (bill_info_id);
 
 COMMIT;
